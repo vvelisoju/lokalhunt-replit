7f9be717e7566f5d953a5565c733097005ca9927
Venkatesh V <119055719+VVelisoju@users.noreply.github.com>
2025-08-23 10:04:56 +0530

    misc

--- server/prisma/migrations/20250820180711_add_pending_approval_status/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "public"."SubscriptionStatus" ADD VALUE 'PENDING_APPROVAL';
--- server/prisma/migrations/20250821090736_add_is_default_to_companies/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."companies" ADD COLUMN     "is_default" BOOLEAN NOT NULL DEFAULT false;
